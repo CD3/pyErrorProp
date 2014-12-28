6d7c9567aa1765b551531269f6c1e1639c2a45ac
C.D. Clark III <clifton.clark@gmail.com>
2014-12-27 21:00:25 -0600

    moved several functions to the error propagation base class

--- pyErrorProp/pyErrorProp.py
+++ pyErrorProp/pyErrorProp.py
@@ -4,6 +4,7 @@ import uncertainties
 import pint
 import numpy
 import copy
+import collections
 
 units = pint.UnitRegistry()
 
@@ -68,49 +69,85 @@ def make_unc_quant( nom, unc ):
 
 
 class ErrorPropagator:
-  pass
-
-class PositiveIntervalPropagator( ErrorPropagator ):
   def __init__(self, func = None):
+    self.set_return_uncertainties(True)
     self.setFunc(func)
 
   def setFunc(self, func = None):
     self.func = func
 
-  def __call__(self, *args, **kargs):
-    return PositiveIntervalPropagator.propagate(self.func,*args,**kargs)
+  def set_return_uncertainties(self, v):
+    self.return_uncertainties = v
 
   @staticmethod
-  def propagate(func, *args, **kargs):
+  def sort_uncertainties(u):
+    if isinstance( u, dict ) or isinstance( u, collections.OrderedDict ):
+      return collections.OrderedDict( sorted( u.items() , reverse = True, key = lambda it : numpy.abs( it[1] ) ) )
+
+    return u
+
+  @staticmethod
+  def relative_uncertainties( uncertainties ):
+    '''Determine the relative contribution of each uncertainty to the total uncertainty.'''
+    total_unc = ErrorPropagator.total_uncertainty( uncertainties )
+    relative_unc = dict()
+    for k,v in uncertainties.items():
+      relative_unc[k] = (total_unc - ErrorPropagator.total_uncertainty( collections.OrderedDict( filter( lambda it: it[0] != k, uncertainties.items() ) ) )  ) / total_unc
+
+    return relative_unc
+
+  @staticmethod
+  def total_uncertainty( uncertainties ):
+    '''Compute and return the total uncertainty from individual uncertainty contributions.'''
+    if isinstance(uncertainties,dict) or isinstance(uncertainties,collecitons.OrderedDict) :
+      uncs = uncertainties.values()
+    else:
+      uncs = uncertainties.values()
+    return numpy.sqrt( sum( [ x*x for x in uncs ] ) )
+
+  def __call__(self, *args, **kargs):
+    nominal_value, uncertainties = self.propagate_uncertainties( *args, **kargs )
+    uncertainty   = ErrorPropagator.total_uncertainty( uncertainties )
+    if self.return_uncertainties:
+      return ( make_unc_quant( nominal_value, uncertainty ), uncertainties)
+    else:
+      return make_unc_quant( nominal_value, uncertainty )
+
+
+
+
+class PositiveIntervalPropagator( ErrorPropagator ):
+  def __init__(self, *args, **kargs):
+    super().__init__( *args, **kargs )
+
+
+  def propagate_uncertainties(self, *args, **kargs):
 
     nominal_args = []
     for i,a in enumerate(args):
       nominal_args.append( nominal(a) )
 
     nominal_kargs = dict()
-    for k,v in kargs.iteritems():
+    for k,v in kargs.items():
       nominal_kargs[k] = nominal( v )
 
     
-    nominal_value = func(*nominal_args,**nominal_kargs)
+    nominal_value = self.func(*nominal_args,**nominal_kargs)
 
-    uncertainties = {}
+    uncertainties = dict()
 
     for i in range(len(args)):
       myargs = copy.copy(nominal_args)
       myargs[i] = upper( args[i] )
-      uncertainties[i] = func(*myargs,**nominal_kargs) - nominal_value
+      uncertainties[i] = self.func(*myargs,**nominal_kargs) - nominal_value
 
     for karg in kargs:
       mykargs = copy.copy(nominal_kargs)
       mykargs[karg] = upper( kargs[karg] )
-      uncertainties[karg] = func(*nominal_args,**mykargs) - nominal_value
+      uncertainties[karg] = self.func(*nominal_args,**mykargs) - nominal_value
 
-    uncertainty = numpy.sqrt( sum( [ x*x for x in uncertainties.values() ] ) )
-
-    
-
-    return ( make_unc_quant( nominal_value, uncertainty ), uncertainties)
+    #return {'nom': nominal_value, 'unc' : uncertainties }
+    return (nominal_value, uncertainties)
 
 
 
