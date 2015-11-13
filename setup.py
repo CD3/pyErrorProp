6df6abf69cdacb99af8f3782d25bd07e2ce968b1
C.D. Clark III <clifton.clark@gmail.com>
2015-11-13 16:35:59 -0600

    workign setup.py now

--- setup.py
+++ setup.py
@@ -2,21 +2,17 @@
 
 import os
 from setuptools import setup, find_packages
-import versioneer
 
 DESCRIPTION = "Error propgation calculations with uncertian quantities."
 LONG_DESCRIPTION = open('README.md').read()
 
-scripts = [ os.path.join('scripts',f) for f in os.listdir('scripts/') if not f.startswith('.') ]
 setup(name='pyErrorProp',
-      version=versioneer.get_version(),
-      cmdclass=versioneer.get_cmdclass(),
+      version='0.0.0',
       description=DESCRIPTION,
       long_description=LONG_DESCRIPTION,
       author='C.D. Clark III',
-      url='https://github.com/CD3/pyLabCalc',
+      url='https://github.com/CD3/pyErrorProp',
       license="MIT License",
       platforms=["any"],
-      packages=find_packages(),
-      scripts=scripts
+      py_modules=['pyErrorProp']
      )
