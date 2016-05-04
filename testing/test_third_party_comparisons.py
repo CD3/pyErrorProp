12d35f0771e2f29d95a36ac500d19a949a4fb99d
C.D. Clark III <clifton.clark@gmail.com>
2016-05-03 22:24:52 -0500

    added tests to compare to the uncertainties module

--- testing/test_third_party_comparisons.py
+++ testing/test_third_party_comparisons.py
@@ -16,7 +16,23 @@ def test_uncertainties_comparison():
   yy = ufloat( 2.5, 0.5 )
   ww = xx
 
+  z = x+y
+  zz = xx+yy
+  assert Close( z.nominal.magnitude, zz.nominal_value, 1e-5 )
+  assert Close( z.uncertainty.magnitude, zz.std_dev, 1e-5 )
+
+  z = x-y/2
+  zz = xx-yy/2
+  assert Close( z.nominal.magnitude, zz.nominal_value, 1e-5 )
+  assert Close( z.uncertainty.magnitude, zz.std_dev, 1e-5 )
+
   z = x*y
   zz = xx*yy
+  assert Close( z.nominal.magnitude, zz.nominal_value, 1e-5 )
+  assert Close( z.uncertainty.magnitude, zz.std_dev, 1e-5 )
 
-  print z,zz
+  z = x/y
+  zz = xx/yy
+  assert Close( z.nominal.magnitude, zz.nominal_value, 1e-5 )
+  # linear approximation differs here!
+  assert not Close( z.uncertainty.magnitude, zz.std_dev, 1e-5 )
