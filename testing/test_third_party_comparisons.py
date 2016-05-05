00ea70592cb448dd4fd6e865c6a472c71ede4b39
C.D. Clark III <clifton.clark@gmail.com>
2016-05-05 15:21:47 -0500

    added comparisons to mcerp.

--- testing/test_third_party_comparisons.py
+++ testing/test_third_party_comparisons.py
@@ -1,8 +1,7 @@
 from pyErrorProp import UncertaintyConvention
-from uncertainties import ufloat
 from Utils import *
 
-import timeit
+import timeit,math
 import pytest
 
 uconv = UncertaintyConvention()
@@ -10,6 +9,7 @@ UQ_ = uconv.UncertainQuantity
 Q_  = UQ_.Quantity
 
 def test_uncertainties_comparison():
+  from uncertainties import ufloat
   # compare error propagation.
   x = UQ_( '2.5 +/- 0.5 m' )
   y = UQ_( '2.5 +/- 0.5 m' )
@@ -53,13 +53,13 @@ def test_uncertainties_comparison():
   zz = xx - yy
   assert Close( z.nominal.magnitude, 0, 1e-5 )
   assert Close( z.nominal.magnitude, zz.nominal_value, 1e-5 )
-  # not sure how to add correlation for two existing variables.
+  # can't correlate variables, only affinescalarfuncs
   assert not Close( z.uncertainty.magnitude, zz.std_dev, 1e-5 )
 
 
-  N = 2
-  data = [ UQ_('2 +/- 0.1 m') ] * N
-  ddata = [ ufloat(2,0.1) ] * N
+  num = 2
+  data = [ UQ_('2 +/- 0.1 m') ] * num
+  ddata = [ ufloat(2,0.1) ] * num
 
   z = sum(data)
   zz = sum(ddata)
@@ -70,9 +70,9 @@ def test_uncertainties_comparison():
   assert Close( z.uncertainty.magnitude, zz.std_dev, 1e-5 )
 
 
-  N = 10
-  data = [ UQ_('2 +/- 0.1 m') ] * N
-  ddata = [ ufloat(2,0.1) ] * N
+  num = 10
+  data = [ UQ_('2 +/- 0.1 m') ] * num
+  ddata = [ ufloat(2,0.1) ] * num
 
   z = sum(data)
   zz = sum(ddata)
@@ -81,3 +81,83 @@ def test_uncertainties_comparison():
   assert Close( z.uncertainty.magnitude, (10*0.1), 1e-5 )
   assert Close( z.nominal.magnitude, zz.nominal_value, 1e-5 )
   assert Close( z.uncertainty.magnitude, zz.std_dev, 1e-5 )
+
+
+
+def test_mcerp_comparison():
+  import mcerp
+  from mcerp import N
+  # bump up the number of samples that will be used
+  mcerp.npts = 100000
+  # compare error propagation.
+  x = UQ_( '2.5 +/- 0.5 m' )
+  y = UQ_( '2.5 +/- 0.5 m' )
+  w = x
+
+  xx = N( 2.5, 0.5 )
+  yy = N( 2.5, 0.5 )
+  ww = xx
+
+  z = x+y
+  zz = xx+yy
+  assert Close( z.nominal.magnitude, zz.mean, 1e-2 )
+  assert Close( z.uncertainty.magnitude, math.sqrt(zz.var), 1e-2 )
+
+  z = x-y/2
+  zz = xx-yy/2
+  assert Close( z.nominal.magnitude, zz.mean, 1e-2 )
+  assert Close( z.uncertainty.magnitude, math.sqrt(zz.var), 1e-2 )
+
+  z = x*y
+  zz = xx*yy
+  assert Close( z.nominal.magnitude, zz.mean, 1e-2 )
+  assert Close( z.uncertainty.magnitude, math.sqrt(zz.var), 1e-2 )
+
+  z = x/y
+  zz = xx/yy
+  # expected value is not actually 1...
+  assert not Close( z.nominal.magnitude, zz.mean, 1e-2 )
+  assert not Close( z.uncertainty.magnitude, math.sqrt(zz.var), 1e-2 )
+
+
+  z = w - x
+  zz = ww - xx
+  assert Close( z.nominal.magnitude, 0, 1e-2 )
+  # mc based method will never get exactly zero
+  # assert Close( z.nominal.magnitude, zz.mean, 1e-2 )
+  assert Close( z.uncertainty.magnitude, math.sqrt(zz.var), 1e-2 )
+
+  # add correlation
+  x.correlated(y,1)
+  z = x - y
+  zz = xx - yy
+  assert Close( z.nominal.magnitude, 0, 1e-2 )
+  # assert Close( z.nominal.magnitude, zz.mean, 1e-2 )
+  # can't correlate variables, only affinescalarfuncs
+  assert not Close( z.uncertainty.magnitude, math.sqrt(zz.var), 1e-2 )
+
+
+  num = 2
+  data = [ UQ_('2 +/- 0.1 m') ] * num
+  ddata = [ N(2,0.1) ] * num
+
+  z = sum(data)
+  zz = sum(ddata)
+
+  assert Close( z.nominal.magnitude, 2*2, 1e-2 )
+  assert Close( z.uncertainty.magnitude, (2*0.1), 1e-2 )
+  assert Close( z.nominal.magnitude, zz.mean, 1e-2 )
+  assert Close( z.uncertainty.magnitude, math.sqrt(zz.var), 1e-2 )
+
+
+  num = 10
+  data = [ UQ_('2 +/- 0.1 m') ] * num
+  ddata = [ N(2,0.1) ] * num
+
+  z = sum(data)
+  zz = sum(ddata)
+
+  assert Close( z.nominal.magnitude, 10*2, 1e-2 )
+  assert Close( z.uncertainty.magnitude, (10*0.1), 1e-2 )
+  assert Close( z.nominal.magnitude, zz.mean, 1e-2 )
+  assert Close( z.uncertainty.magnitude, math.sqrt(zz.var), 1e-2 )
